47f6f82f3d4033accd6208fb340ff788250d6171
Yichao Wu <wuyichao71@126.com>
2025-05-13 17:37:05 +0900

    start of 2021-w1

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
-const main = () => {
+let renderTime = 0;
+let SNAKE_SPEED = 2;
+
+const main = (currentTime) => {
+    window.requestAnimationFrame(main);
+    const secondSinceRender = (currentTime - renderTime) / 1000;
+    if (secondSinceRender < 1 / SNAKE_SPEED) {
+        return;
+    }
+    renderTime = currentTime;
+    console.log(currentTime);
     update();
     draw();
 }
 
-setInterval(main);
+// setInterval(main);
+window.requestAnimationFrame(main);
 
 const update = () => {
 }
 
 const draw = () => {
+    let board = document.getElementById('game-board');
+    board.innerHTML = "";
+    let aDiv = document.createElement('div');
+    aDiv.style.background = 'red';
+    board.appendChild(aDiv);
 }
