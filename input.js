c4f16822ad724f1f1db7fc9ff4f8de574af1cef0
Yichao Wu <wuyichao71@126.com>
2025-05-14 01:13:36 +0900

    still can not move in input.js: modify input.js

--- input.js
+++ input.js
@@ -1,20 +1,6 @@
 let inputDirection = {x: 0, y: 1};
 
 window.addEventListener("keydown", (event) => {
-    // switch (event.key) {
-    //     case "arrowDown":
-    //         inputDirection = {x: 0, y: 1};
-    //         break;
-    //     case "arrowUp":
-    //         inputDirection = {x: 0, y: -1};
-    //         break;
-    //     case "arrowLeft":
-    //         inputDirection = {x: -1, y: 0};
-    //         break;
-    //     case "arrowRight":
-    //         inputDirection = {x: 1, y: 0};
-    //         break;
-    // }
     if (event.key === "arrowUp") {
         inputDirection = {x: 0, y: -1};
     } else if (event.key === "arrowDown") {
