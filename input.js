2f2afafe2ec85bf9fafed0cf3a09ca4daf28e0e1
Yichao Wu <wuyichao71@126.com>
2025-05-15 12:21:34 +0900

    index.html: add direction botton

--- input.js
+++ input.js
@@ -13,23 +13,49 @@ window.addEventListener("keydown", (event) => {
     }
 })
 
-window.addEventListener("click", event => {
-    const centerX = window.innerWidth / 2;
-    const centerY = window.innerHeight / 2;
-    if (lastInputDirection.x !== 0) {
-        if (event.y > centerY) {
-            inputDirection = {x: 0, y: 1};
-        } else {
-            inputDirection = {x: 0, y: -1};
-        }
-    } else if (lastInputDirection.y != 0) {
-        if (event.x < centerX) {
-            inputDirection = {x: -1, y: 0};
-        } else {
-            inputDirection = {x: 1, y: 0};
-        }
+// window.addEventListener("pointerdown", event => {
+//     const centerX = window.innerWidth / 2;
+//     const centerY = window.innerHeight / 2;
+//     if (lastInputDirection.x !== 0) {
+//         if (event.clientY > centerY) {
+//             inputDirection = {x: 0, y: 1};
+//         } else {
+//             inputDirection = {x: 0, y: -1};
+//         }
+//     } else if (lastInputDirection.y != 0) {
+//         if (event.clientX < centerX) {
+//             inputDirection = {x: -1, y: 0};
+//         } else {
+//             inputDirection = {x: 1, y: 0};
+//         }
+//     }
+//     const showPos = document.getElementById("show-pos");
+//     showPos.innerText = `centerX: ${centerX}; centerY: ${centerY}\nclientX: ${event.clientX}; clientY: ${event.clientY}`;
+// })
+
+const upDirection = () => {
+    if (lastInputDirection.x != 0) {
+        inputDirection = {x: 0, y: -1};
     }
-})
+}
+
+const rightDirection = () => {
+    if (lastInputDirection.y != 0) {
+        inputDirection = {x: 1, y: 0};
+    }
+}
+
+const downDirection = () => {
+    if (lastInputDirection.x != 0) {
+        inputDirection = {x: 0, y: 1};
+    }
+}
+
+const leftDirection = () => {
+    if (lastInputDirection.y != 0) {
+        inputDirection = {x: -1, y: 0};
+    }
+}
 
 const getInputDirection = () => {
     lastInputDirection = inputDirection;
